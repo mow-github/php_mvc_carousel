6e47db46bd14ef12b79634277b0d783e53d729bc
Mats Wikmar <matwik@gmail.com>
2016-12-05 16:48:21 +0100

    v1

--- php/model/Model.php
+++ php/model/Model.php
@@ -25,7 +25,7 @@ class Model{
     try {
       $this->openConnectionDB();
 
-      $pdoStatement = $this->pdocon->prepare("SELECT * FROM image");
+      $pdoStatement = $this->pdocon->prepare("SELECT srcOrg, srcMini, text, CONCAT(fname,' ', lname) AS name FROM image");
       $pdoStatement->execute();
       $tablecontent = $pdoStatement->fetchAll(PDO::FETCH_OBJ);
       $this->pdocon = NULL;
@@ -39,9 +39,9 @@ class Model{
   public function getImagesMOCK(){
     try {
       $object = [0 =>
-        (object) ["image_id" => "1","srcOrg" => "https://dummyimage.com/1920x1920/000000/fff", "srcMini" => "https://dummyimage.com/480x480/000000/fff", "text" => "image 1"],
-        (object) ["image_id" => "2","srcOrg" => "https://dummyimage.com/1920x1920/6b226b/fff", "srcMini" => "https://dummyimage.com/480x480/6b226b/fff", "text" => "image 2"],
-        (object) ["image_id" => "3","srcOrg" => "https://dummyimage.com/1920x1920/222869/fff", "srcMini" => "https://dummyimage.com/480x480/222869/fff", "text" => "image 3"]
+        (object) ["image_id" => "1","srcOrg" => "https://dummyimage.com/1920x1920/000000/fff", "srcMini" => "https://dummyimage.com/480x480/000000/fff", "text" => "image 1", "name" => "Donald Duck"],
+        (object) ["image_id" => "2","srcOrg" => "https://dummyimage.com/1920x1920/6b226b/fff", "srcMini" => "https://dummyimage.com/480x480/6b226b/fff", "text" => "image 2", "name" => "Donald Duck"],
+        (object) ["image_id" => "3","srcOrg" => "https://dummyimage.com/1920x1920/222869/fff", "srcMini" => "https://dummyimage.com/480x480/222869/fff", "text" => "image 3", "name" => "Donald Duck"]
       ];
       return $object;
     } catch (PDOException $pdoexp) {
